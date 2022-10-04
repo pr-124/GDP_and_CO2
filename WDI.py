6fc69e52e17605637585791512ec91d4001404da
ruhamyakmc <ruhamyaemmanuel@gmail.com>
2022-10-04 12:06:46 -0400

    modified wdi.py

--- WDI.py
+++ WDI.py
@@ -8,3 +8,10 @@ mortality = wdi['Mortality rate, infant (per 1,000 live births)']git
 GDP = wdi['GDP per capita (constant 2010 US$)']
 country = wdi['Country Name']
 plt.plot(GDP, mortality)
+
+plt.xlabel('GDP per capita (constant 2010 US$)')
+plt.ylabel('Mortality rate, infant (per 1,000 live births)')
+plt.title('Mortality rate vs. GDP per capita')
+plt.show()
+
+# GDP per capita vs. Mortality rate, infant (per 1,000 live births)
